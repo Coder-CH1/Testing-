b10d7ffe131b9dccd9cbf7340ea60eea0d1cd6ee
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-08-05 11:35:05 +0100

    added

--- lib/integration_test.dart
+++ lib/integration_test.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:testing/main.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('', (widgetTester) async {
+   await widgetTester.pumpWidget(MyApp());
+   expect(find.text('Test'), findsOneWidget);
+  });
 }
